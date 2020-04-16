1632106cced6baa6b6aa743ee363ccda99b329f5
XianGuo <1679905582@qq.com>
2020-04-16 16:17:24 +0900

    添加项目说明

--- src/com/xianguo/ftp/core/FtpCore.java
+++ src/com/xianguo/ftp/core/FtpCore.java
@@ -4,7 +4,5 @@ public class FtpCore {
 	public static void main(String[] args) throws Exception{
 		FtpCommandCore commandCore = new FtpCommandCore();
 		commandCore.start();
-		/*FtpStreamCore streamCore = new FtpStreamCore();
-		streamCore.start();*/
 	}
 }
